e86e9581f00c555baab90ea522239d480e997882
visualczm <448443959@qq.com>
2017-09-17 04:06:25 +0800

    modif

--- frontend/modules/site/views/default/index.php
+++ frontend/modules/site/views/default/index.php
@@ -425,8 +425,8 @@ layui.use(['layer'], function () {
                         {
                             $('#wechat_uid').val(data.data.openid);
                         }else{
-                            var lefts=($(window).width() - $('#qrcode').outerWidth()-78.5)/2;
-                            var tops=($(window).height() - $('#qrcode').outerHeight())/2 + $(document).scrollTop();
+                            var lefts=($(window).width() - 170)/2;
+                            var tops=($(window).height() -196)/2 + $(document).scrollTop();
 
                             layer.open({
                                 offset:[tops+'px',lefts+'px'],
