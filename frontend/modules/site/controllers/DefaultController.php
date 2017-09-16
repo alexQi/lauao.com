9c8ba6c783a5e3d9c5ade012f77faf41991f1df1
alex <alex.qiubo@qq.com>
2017-09-16 23:22:36 +0800

    modify

--- frontend/modules/site/controllers/DefaultController.php
+++ frontend/modules/site/controllers/DefaultController.php
@@ -28,11 +28,12 @@ class DefaultController extends BaseController
 
         $getWechatTokenUrl = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.yii::$app->params['wechat_appid'].'&secret='.yii::$app->params['wechat_secret'].'&code='.yii::$app->request->get('code').'&grant_type=authorization_code';
         $wechatToken = Common::httpRequest($getWechatTokenUrl);
+        var_dump($wechatToken);
         return $this->render('index',[
             'advertList' => $advertList,
             'activityInfo' => $activityInfo,
             'serverTime' => $serverTime,
-            'activity' => $activity
+            'activity' => $activity,
         ]);
     }
 
