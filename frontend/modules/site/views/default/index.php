3801d7efd409b77043924933a34ba7bd68ccd794
visualczm <448443959@qq.com>
2017-09-17 16:16:24 +0800

    modif

--- frontend/modules/site/views/default/index.php
+++ frontend/modules/site/views/default/index.php
@@ -677,7 +677,7 @@ $(".seachperson").on("click", ".person", function (e) {
                         $('.search-apply-desc').html('介紹:' + data.data.self_desc);
                         $('.search-apply-vote').html(data.data.votes);
                         $('.search-apply-image').prop('src', data.data.self_picture);
-                        $('#search-apply-audio').load('<source src="'+data.data.self_media+'" type="audio/mpeg">');
+                        $('#search-apply-audio').html('<source src="'+data.data.self_media+'" type="audio/mpeg">');
                        // var midea=$('#search-apply-audio')[0];
                        // midea.currentSrc=data.data.self_media; //获取对象将值赋给对象
                     
