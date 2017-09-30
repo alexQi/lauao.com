d964d3d64488f650534a6f626b8f08cbba39dc82
alex <alex.qiubo@qq.com>
2017-09-30 23:24:17 +0800

    ..

--- common/models/Pay/Wechat/WxPayApi.php
+++ common/models/Pay/Wechat/WxPayApi.php
@@ -54,8 +54,10 @@ class WxPayApi
         //签名
         $inputObj->SetSign();
         $xml = $inputObj->ToXml();
+        var_dump($inputObj);
+        var_dump($xml);
 
-        var_dump($xml);die();
+        die();
 
         $startTimeStamp = self::getMillisecond();//请求开始时间
         $response = self::postXmlCurl($xml, $url, false, $timeOut);
