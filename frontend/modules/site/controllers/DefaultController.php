2473ee0687bfd01e077e7b501060af764dbd1f7c
alex <alex.qiubo@qq.com>
2017-10-01 12:00:43 +0800

    modify

--- frontend/modules/site/controllers/DefaultController.php
+++ frontend/modules/site/controllers/DefaultController.php
@@ -87,7 +87,7 @@ class DefaultController extends BaseController
 
         $api = new ApiService();
         if ($jsApiParameters = $api->wxpayCreateOrder($orderInfo)){
-            return $this->render($this->view .'do-wechat',['payConfig'=>$jsApiParameters]);
+            return $this->render('do-wechat',['payConfig'=>$jsApiParameters]);
         }else{
             throw new ForbiddenHttpException('生成微信订单失败');
         }
