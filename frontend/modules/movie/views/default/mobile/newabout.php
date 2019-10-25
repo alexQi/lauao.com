6624843e3208d399bff042cf4d00a75504938adc
visualczm <448443959@qq.com>
2019-10-25 22:02:12 +0800

    添加皖网文号

--- frontend/modules/movie/views/default/mobile/newabout.php
+++ frontend/modules/movie/views/default/mobile/newabout.php
@@ -15,6 +15,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="./layui/css/layui.css">
     <script src="./layui/layui.js"></script>
+    <style>
+        .footer{
+            position: fixed;
+            bottom: 0px;
+            width: 100%;
+            height: 30px;
+            padding: 5px;
+            border-top: 1px solid #801e99;
+            text-align: center;
+            color: white;
+            background: #031871;
+
+            line-height: 30px;
+        }
+    </style>
 </head>
 <body>
 <div >
@@ -24,7 +39,8 @@
                 <img class="layui-col-xs12" src="http://image.sboyo.com/ahwes_app_images.jpg" />
             </div>
 
-        </div>
+    </div>
+    <div class="footer" >皖ICP备17005514号-1 皖网文[2019] 4652-199 号</div>
 
 
 
