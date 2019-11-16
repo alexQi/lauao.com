e12a03d0c4153c503c39f946614d86869f4b9a90
visualczm <448443959@qq.com>
2019-11-16 22:11:34 +0800

    添加皖网文号

--- frontend/modules/movie/views/default/mobile/newabout.php
+++ frontend/modules/movie/views/default/mobile/newabout.php
@@ -9,14 +9,14 @@ use common\models\Pay\Wechat;
 
 ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<html>
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>宣城直播创作团队</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <link rel="stylesheet" href="./layui/css/layui.css">
-    <script src="./layui/layui.js"></script>
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1">-->
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">-->
+<!--    <link rel="stylesheet" href="./layui/css/layui.css">-->
+<!--    <script src="./layui/layui.js"></script>-->
     <style>
         .footer{
             position: fixed;
