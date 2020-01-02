9c52c0744c1da55d86fa3441f09e7c53e4ea7be5
visualczm <448443959@qq.com>
2020-01-02 18:55:15 +0800

    新增活动页面

---
+++ frontend/modules/movie/views/default/mobile/activity.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 44844
+ * Date: 2019/8/18
+ * Time: 15:36
+ */
+use common\models\Pay\Wechat;
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>维尔斯直播团队</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="./layui/css/layui.css">
+    <script src="./layui/layui.js"></script>
+    <style>
+        .footer{
+            position: fixed;
+            bottom: 0px;
+            width: 100%;
+            height: 30px;
+            padding: 5px;
+            border-top: 1px solid #801e99;
+            text-align: center;
+            color: white;
+            background: #031871;
+
+            line-height: 30px;
+        }
+    </style>
+</head>
+<body>
+<div >
+
+    <div class="layui-row" >
+        <div >
+            <img class="layui-col-xs12" src="http://images.ahwes.com/activity.jpg" />
+        </div>
+
+    </div>
+<!--    <div class="footer" >皖ICP备17005514号-1 皖网文[2019] 4652-199 号</div>-->
+
+</div>
+</body>
